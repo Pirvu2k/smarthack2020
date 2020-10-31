204fd6b12d93ae9d4b7ffc2877ccbd7b01afbf55
Andrei250 <andrei_dumitrescu2000@yahoo.ro>
2020-10-31 16:35:12 +0200

    Added tinyMCE

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/paper/create', 'PaperController@create')->name('paper.create');
+Route::post('/paper/create', 'PaperController@addPaper')->name('paper.creation');
