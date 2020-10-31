f726d665dc1d503c77ea09a9c770811e808f66ff
Pirvu2k <mitapirvuet@gmail.com>
2020-11-01 01:28:17 +0200

    fix buttons

--- resources/views/paper/list.blade.php
+++ resources/views/paper/list.blade.php
@@ -3,49 +3,28 @@
 @section('content')
     <div class="container">
         @foreach($userDocuments as $uDoc)
-            @if (!$uDoc->document->hasFiles())
                 <div class="card mt-2">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-10">
+                            <div class="col-md-8">
                                 <p>{{$uDoc->name}}</p>
                                 <p>Completat la: {{$uDoc->created_at}}</p>
                             </div>
-                            <div class="col-md-2">
+                            <div class="col-md-4">
                                 <div class="float-right">
-                                    <a href="{{asset('storage/'. $uDoc->path)}}" target="_blank "><button class="btn btn-primary">Vezi Documentul <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                                <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
-                                            </svg></button>
+                                    <a href="{{asset('storage/'. $uDoc->path)}}" target="_blank "><button class="btn btn-primary">Vezi Documentul</button>
                                     </a>
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </div>
-                </div>
-
-            @else
-
-                <div class="card mt-2">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-10">
-                                <p>{{$uDoc->name}}</p>
-                                <p>Completat la: {{$uDoc->created_at}}</p>
-                            </div>
-                            <div class="col-md-2">
-                                <div class="float-right">
-                                    <button aria-controls="collapseExample{{$uDoc->id}}" aria-expanded="false" class="btn btn-primary"
+                                    @if ($uDoc->document->hasFiles())
+                                    <button aria-controls="collapseExample{{$uDoc->id}}" aria-expanded="false" class="btn btn-success"
                                             data-target="#collapseExample{{$uDoc->id}}" data-toggle="collapse"
                                             type="button">
                                         Vezi fisiere
                                     </button>
+                                    @endif
                                 </div>
                             </div>
                         </div>
-
+                        @if ($uDoc->document->hasFiles())
                         <div class="collapse" id="collapseExample{{$uDoc->id}}">
                             <div class="card card-body">
                                 @foreach($uDoc->userAdditionalFiles as $file)
@@ -56,10 +35,10 @@
                                 @endforeach
                             </div>
                         </div>
+                        @endif
                     </div>
                 </div>
 
-            @endif
         @endforeach
     </div>
 @endsection
