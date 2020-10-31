be7c68d5907ccf6903695e0507783b4090d6d931
Ion Duiu <duiuionut0@gmail.com>
2020-10-31 19:38:04 +0200

    added convertor for html to pdf

--- app/Http/Controllers/PaperController.php
+++ app/Http/Controllers/PaperController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Document;
+use Spipu\Html2Pdf\Html2Pdf;
 
 class PaperController extends Controller
 {
     public function create(Request $request) {
         return view('paper.create');
+    }
+
+    public function pdf($id) {
+        $document = Document::find($id);
+        $html2pdf = new Html2Pdf('P', 'A4', 'en', true, 'UTF-8', array(16, 16, 12, 8), true);
+        $html2pdf->writeHTML($document->content);
+        return $html2pdf->output($document->title.'.pdf');
     }
 
     public function addPaper(Request $request) {
